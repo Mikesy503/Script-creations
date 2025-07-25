5720dfa4f76f45bf1a2881d72ffd7cef856816c2
Mikesy503 <networkmikeandyou@gmail.com>
2025-07-24 20:52:17 -0700

    Add files via upload

---
+++ Appinstaller.ps1
@@ -0,0 +1,7 @@
+#need to install a list of programs
+<# list of programs so far
+Discord, Steam, vscode, powershell 7, github, #>
+
+winget install discord.discord
+winget install valve.steam
+Invoke-Item -Path C:\program` files` `(x86`)\steam\steam.exe
